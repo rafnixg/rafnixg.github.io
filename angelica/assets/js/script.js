4fa9f88819a057af23995f1206b53c60fca18a3f
rafnix <rafnixg@gmail.com>
2016-02-09 17:16:17 -0430

    Actualizacion

--- angelica/assets/js/script.js
+++ angelica/assets/js/script.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 	$('#countup').countup({
-    	start: new Date(2010, 5, 5, 17, 42, 21), //year, month, day, hour, min, sec
+    	start: new Date(2010, 4, 11, 17, 42, 21), //year, month, day, hour, min, sec
   
 	});
 	
---
+++ angelica/assets/js/script.js~
@@ -0,0 +1,8 @@
+$(function(){
+	
+	$('#countup').countup({
+    	start: new Date(2010, 5, 5, 17, 42, 21), //year, month, day, hour, min, sec
+  
+	});
+	
+});
