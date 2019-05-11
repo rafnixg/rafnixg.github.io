73cf32a9f2f2a6651391de1870046db4385fdd0c
Rafnixg <rafnix.guzman@sead.pe>
2019-05-10 20:26:24 -0500

    first commit - FIX

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,7 +18,7 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = 'feeds/%s.rss.xml'
+AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
 RSS_FEED_SUMMARY_ONLY = False
 
